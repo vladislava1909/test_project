1dd119df61fcf558abc05dbeac7a3c5e42ba8ef9
System Administrator <root@vanya.darnet.ru>
2022-03-09 14:45:59 +0300

    2 commit

---
+++ may.py
@@ -0,0 +1,10 @@
+#text = input("Введите текст:")
+
+#unique = list(set(text))
+
+#print("Количество уникальных символов: ", len(unique))
+L = ['a', 'b', 'c']
+print(id(L))
+
+L.append('d')
+print(id(L))
