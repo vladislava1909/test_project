ac9fc0aca45f5748ef969d198a3f639cfc0183c4
System Administrator <root@vanya.darnet.ru>
2022-03-12 17:13:48 +0300

    tasks

--- may.py
+++ may.py
@@ -56,4 +56,31 @@ list_id_after = id(shopping_center[-1])
 
 print(list_id_before == list_id_after)
 '''
+'''
+# можно проверить, находится ли число 1 в промежутке (0,4)
+cond1 = 0 < 1
+cond2 = 1 < 4
+
+print(cond1 and cond2)
+# True
+
+# или, например, содержат ли две строки один и тот же символ
+cond3 = 't' in "python"
+cond4 = 't' in "django"
+
+print(cond3 and cond4)
+# False
+'''
+'''
+print(not True)
+# False
+
+print(not False)
+# True
+'''
+'''
+# к слову, логические выражения можно сразу объединять в одно целое
+print(('t' in "python") or ('t' in "django"))
+# True
+'''
 
