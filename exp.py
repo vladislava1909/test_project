98386bdad7b2028519e08af076eea2e2b619e6a0
System Administrator <root@vanya.darnet.ru>
2022-03-29 18:39:52 +0300

    vebinar_files

--- exp.py
+++ exp.py
@@ -1,2 +1,42 @@
+print("8. работа с JSON файлами. Импорт модуля json.")
+
+import json
+
+with open('vebinar_files/json_example.json', encoding='utf8') as f:
+    strfile = f.read()
+    templates = json.loads(strfile)
+
+print(templates)
+print(type(templates))
 
 
+template = {
+    'firstname': 'Иван',
+    'lastname': 'Иванов',
+    'isAlive': True,
+    'age': 32,
+    'address': {
+        'streetAddress': 'Нейбута 32',
+        'city': 'Владивосток',
+        'state': '',
+        'postalcode': ''
+    },
+    'phoneNumbers': [
+        {
+            'type': 'mob',
+            'number': '123-333-4455'
+        },
+        {
+            'type': 'office',
+            'number': '123 111-4567'
+        }
+    ],
+    'children': [],
+    'spouse': None
+}
+
+with open('venv/to_json_example.json', 'w', encoding='utf8') as f:
+    json.dump(template, f, ensure_ascii=False, indent=4)
+
+with open('venv/to_json_example.json', encoding='utf8') as f:
+    print(f.read())
--- work_with_files.py
+++ work_with_files.py
@@ -77,3 +77,49 @@ with open("output.txt", 'w', encoding="utf8") as myFile:
     myFile.write(summary)
 
 
+
+print("8. работа с JSON файлами. Импорт модуля json.")
+
+import json
+
+with open('vebinar_files/json_example.json', encoding='utf8') as f:
+    strfile = f.read()
+    templates = json.loads(strfile)
+
+print(templates)
+print(type(templates))
+
+import json
+
+template = {
+    'firstname': 'Иван',
+    'lastname': 'Иванов',
+    'isAlive': True,
+    'age': 32,
+    'address': {
+        'streetAddress': 'Нейбута 32',
+        'city': 'Владивосток',
+        'state': '',
+        'postalcode': ''
+    },
+    'phoneNumbers': [
+        {
+            'type': 'mob',
+            'number': '123-333-4455'
+        },
+        {
+            'type': 'office',
+            'number': '123 111-4567'
+        }
+    ],
+    'children': [],
+    'spouse': None
+}
+
+with open('venv/to_json_example.json', 'w', encoding='utf8') as f:
+    json.dump(template, f, ensure_ascii=False, indent=4)
+
+with open('venv/to_json_example.json', encoding='utf8') as f:
+    print(f.read())
+
+
